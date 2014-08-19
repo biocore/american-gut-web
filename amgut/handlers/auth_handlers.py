385e717239e0ae31785bc2311c9a7555d86c71f9
Joshua Shorenstein <squirrelo@gmail.com>
2014-08-19 16:19:57 -0600

    add validation and processing for registration form

--- amgut/handlers/auth_handlers.py
+++ amgut/handlers/auth_handlers.py
@@ -14,23 +14,94 @@ class AuthRegisterHandoutHandler(BaseHandler):
     @authenticated
     def get(self):
         latlong_db = _get_lat_long()
-        skid = self.get_argument("skid").strip()
+        skid = self.current_user
         self.render("register_user.html", skid=skid, latlongs_db=latlong_db)
 
     @authenticated
     def post(self):
         skid = self.get_argument("kit_id")
         info = {}
-        for info_column in ("email", "name", "address", "city", "state", "zip",
-                            "country"):
-
+        for info_column in ("email", "participantname", "address", "city",
+                            "state", "zip", "country"):
             # Make sure that all fields were entered
             info[info_column] = self.get_argument(info_column, None)
 
-        AG_DATA_ACCESS.addAGLogin(info['email'], info['name'],
+        # create the user
+        AG_DATA_ACCESS.addAGLogin(info['email'], info['participantname'],
                                   info['address'], info['city'],
                                   info['state'], info['zip'],
                                   info['country'])
+
+        # Create the kit
+        ag_login_id = AG_DATA_ACCESS.get_user_for_kit(skid)
+        kitinfo = AG_DATA_ACCESS.getAGKitDetails(skid)
+        printresults = AG_DATA_ACCESS.checkPrintResults(kit_id)
+        if printresults is None:
+            printresults = 'n'
+            success = ag_data_access.addAGKit(
+                ag_login_id, kit_id, password, kitinfo['swabs_per_kit'],
+                kitinfo['kit_verification_code'], printresults)
+
+        # add the kit to the user
+        printresults = ag_data_access.checkPrintResults(kit_id)
+        if printresults is None:
+            printresults = 'n'
+            success = ag_data_access.addAGKit(
+                ag_login_id, kit_id, password, swabs_per_kit,
+                kit_verification_code, printresults)
+        if success == -1:
+            self.redirect('/db_error/?msg=regkit')
+            return
+
+        # Add the barcodes
+        ag_kit_id = kitinfo['ag_kit_id']
+        results = AG_DATA_ACCESS.get_barcodes_from_handout_kit(skid)
+        for row in results:
+            barcode = row[0]
+            success = AG_DATA_ACCESS.addAGBarcode(ag_kit_id, barcode)
+            if success == -1:
+                self.redirect('/db_error/msg=regbarcode')
+                return
+
+
+
+        # Email the verification code
+        subject = "American Gut Verification Code"
+        addendum = ''
+        if kit_id.startswith('PGP_'):
+            addendum = """For the PGP cohort, we are requesting that you collect one sample from each of the following sites:\n\nLeft hand\nRight hand\nForehead\nMouth\nFecal\n\nThis is important to ensure that we have the same types of samples for all PGP participants which, in turn, could be helpful in downstream analysis when looking for relationships between the microbiome and the human genome."""
+
+        body = """
+        Thank you for registering with the American Gut Project! Your verification code is:
+
+        {0}
+
+        You will need this code to verifiy your kit on the American Gut webstite. To get started, please log into:
+
+        http://microbio.me/AmericanGut
+
+        Enter the kit_id and password found inside your kit, verify the contents of your kit, and enter the verification code found in this email.
+
+        {1}
+
+        Sincerely,
+        The American Gut Team
+
+        """.format(kit_verification_code, addendum)
+
+        # Only email on systems that have email capability:
+        try:
+            if can_send_mail():
+                send_email(body, subject, email)
+            else:
+                req.write(format_submit_form_to_fusebox_string(page="portal.psp",
+                    message='Mail can only be sent from microbio.me domain.'))
+        except:
+            req.write(format_submit_form_to_fusebox_string(page="portal.psp",
+                message="There was a problem sending your email "
+                'Please contact us directly at '
+                '<a href=&quot;mailto:info@americangut.org&quot;>info@americangut.org</a>'))
+
 
         self.redirect('/authed/index/')
 
@@ -41,7 +112,6 @@ class AuthLoginHandler(BaseHandler):
         skid = self.get_argument("skid", "").strip()
         password = self.get_argument("password", "")
         login = AG_DATA_ACCESS.authenticateWebAppUser(skid, password)
-        print "LOGIN!!!!!", login
         if login:
             # everything good so log in
             self.set_current_user(skid)
@@ -50,9 +120,9 @@ class AuthLoginHandler(BaseHandler):
         else:
             is_handout = AG_DATA_ACCESS.handoutCheck(skid, password)
             if is_handout == 'y':
-                #login user but have them register themselves
+                # login user but have them register themselves
                 self.set_current_user(skid)
-                self.redirect('/auth/register/?skid=%s' % skid)
+                self.redirect('/auth/register/')
                 return
             else:
                 msg = "Invalid Kit ID or Password"
