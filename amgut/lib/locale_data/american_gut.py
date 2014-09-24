bbb3d713abde522214888beddad52d252cf30dc1
Daniel McDonald <mcdonadt@colorado.edu>
2014-09-23 23:52:47 -0600

    ENH: start of locale structure

---
+++ amgut/lib/locale_data/american_gut.py
@@ -0,0 +1,10 @@
+# Template specific dicts
+_FAQ = {'FAQ_0_WHAT_IS_A_GUT': 'asdasdasd'}
+
+# Actual text locale
+text_locale = {
+        'FAQ.html': _FAQ
+        }
+
+# Any media specific localizations
+media_locale = {'logo': '/static/img/ag_logo.jpg'}
