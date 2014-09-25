e13c9f5691d89a3f6a4de30b1e9d14e039ee2625
Daniel McDonald <mcdonadt@colorado.edu>
2014-09-25 15:27:50 -0600

    LICENSE

--- amgut/lib/locale_data/american_gut.py
+++ amgut/lib/locale_data/american_gut.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+from __future__ import division
+
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014--, The American Gut Project Development Team.
+#
+# Distributed under the terms of the BSD 3-clause License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+
 # Template specific dicts
 _FAQ = {'FAQ_0_WHAT_IS_A_GUT': 'asdasdasd'}
 
