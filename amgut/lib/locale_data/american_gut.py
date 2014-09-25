a83d1fb3debc6d8dfaa47e92b6206007ed7daeed
Daniel McDonald <mcdonadt@colorado.edu>
2014-09-25 16:31:15 -0600

    taxa_summary locale text

--- amgut/lib/locale_data/american_gut.py
+++ amgut/lib/locale_data/american_gut.py
@@ -12,11 +12,15 @@ from __future__ import division
 
 # Template specific dicts
 _FAQ = {'FAQ_0_WHAT_IS_A_GUT': 'asdasdasd'}
+_TAXA_SUMMARY = {'RESOLUTION_NOTE': "Note: Where there are blanks in the table below, the taxonomy could not be resolved in finer detail.",
+                 'PERCENTAGES_NOTE': "Note: The percentages listed represent the relative abundance of each taxon. This summary is based off of normalized data. Because of limitations in the way the samples are processed, we cannot reliably obtain species level resolution. As such, the data shown are collapsed at the genus level.",
+                 'DOWNLOAD_LINK': "Download the table"}
 
 # Actual text locale
 text_locale = {
-        'FAQ.html': _FAQ
-        }
+    'FAQ.html': _FAQ,
+    'taxa_summary.html': _TAXA_SUMMARY
+    }
 
 # Any media specific localizations
 media_locale = {'logo': '/static/img/ag_logo.jpg'}
