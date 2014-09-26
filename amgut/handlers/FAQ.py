34393e698e1c21d1a2c54a92cbe3508360be7765
Joshua Shorenstein <squirrelo@gmail.com>
2014-09-26 17:09:36 -0600

    start adding translations dictionary calls

--- amgut/handlers/FAQ.py
+++ amgut/handlers/FAQ.py
@@ -1,6 +1,10 @@
 from amgut.handlers.base_handlers import BaseHandler
+from amgut import text_locale, media_locale
 
 
 class FAQHandler(BaseHandler):
+    page = 'FAQ.html'
+
     def get(self):
-        self.render('FAQ.html', loginerror='')
+        self.render(self.page, loginerror='', media_locale=media_locale,
+                    text_locale=text_locale[self.page])
