6affae58e892e9f929a5ec47ea68b389b858b334
Daniel McDonald <mcdonadt@colorado.edu>
2014-09-29 13:07:17 -0600

    Retreive kit id locale bits

--- amgut/lib/locale_data/american_gut.py
+++ amgut/lib/locale_data/american_gut.py
@@ -21,10 +21,21 @@ _HELP_REQUEST = {
     'PROBLEM_PROMPT': "Enter information related to your problem"
 }
 
+_RETREIVE_KITID = {
+    'UNKNOWN_EMAIL': 'This email address is not in our system',
+    'ENTER_EMAIL': 'Please Enter Your Email',
+    'SEND_EMAIL': 'Send Kit ID Email',
+    'EMAIL_SUCCESS': 'Your kit ID has been emailed to you. Please check your email.',
+    'EMAIL_CANTSEND': 'Mail can be sent only from microbio.me domain.',
+    'EMAIL_EXCEPTION': 'There was a problem sending you the kit ID. Please contact us directly at <a href=\"mailto:info@americangut.org\">info@americangut.org</a>.',
+    'EMAIL_PROMPT': 'Email:'
+    }
+
 # Actual text locale
 text_locale = {
     'FAQ.html': _FAQ,
-    'help_request.html': _HELP_REQUEST
+    'help_request.html': _HELP_REQUEST,
+    'retrieve_kitid.html': _RETREIVE_KITID
     }
 
 # Any media specific localizations
