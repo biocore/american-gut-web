e4258971ba47d682f27cd453db96bb11780c8359
Daniel McDonald <mcdonadt@colorado.edu>
2014-09-29 11:52:33 -0600

    Fetched and updated forgot password

--- amgut/lib/locale_data/american_gut.py
+++ amgut/lib/locale_data/american_gut.py
@@ -13,9 +13,18 @@ from __future__ import division
 # Template specific dicts
 _FAQ = {'FAQ_0_WHAT_IS_A_GUT': 'asdasdasd'}
 
+_FORGOT_PASSWORD = {'ENTER_ID_EMAIL': 'Enter your Kit ID and email',
+                    'KIT_ID': 'Kit ID:',
+                    'EMAIL': 'E-mail',
+                    'EMAIL_RESET_PASSWORD': 'You will receive an email shortly with instructions to reset your password. Please check your email because you need to reset your password within two hours.',
+                    'EMAIL_FAILED': '<p>There was a problem sending you the password reset code. Please contact us directly at <a href=\"mailto:info@americangut.org\" target=\"_blank\"> info@americangut.org</a>.</p><p>Email contained: </p><p>{{message}}</p>',
+                    'NO_RECORD': '<p style="color:red;">This information does not match our records</p><p>Please email <a href="mailto:info@americangut.org" target="_blank"> for further assistance<p>',
+                    'SEND_EMAIL': 'Send email'}
+
 # Actual text locale
 text_locale = {
-        'FAQ.html': _FAQ
+        'FAQ.html': _FAQ,
+        'forgot_password.html': _FORGOT_PASSWORD
         }
 
 # Any media specific localizations
