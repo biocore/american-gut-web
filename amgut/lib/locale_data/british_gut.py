d8b5505d74c65526745c1631e5d3d5ba9537a91f
Joshua Shorenstein <squirrelo@gmail.com>
2014-10-02 11:23:15 -0600

    Seperate british gut tracking. Fix #147

--- amgut/lib/locale_data/british_gut.py
+++ amgut/lib/locale_data/british_gut.py
@@ -12,7 +12,7 @@ from __future__ import division
 
 # Any media specific localizations
 media_locale = {
-    'ANALYTICS_ID': 'UA-55353353-1',
+    'ANALYTICS_ID': 'UA-55355651-1',
     'LATITUDE': 54.5,
     'LONGITUDE': -3.5,
     'ZOOM': 6,
