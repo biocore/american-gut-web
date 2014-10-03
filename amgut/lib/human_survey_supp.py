ddada823dfe05b4ea74ab8448810ee5c93db6dc1
Joshua Shorenstein <squirrelo@gmail.com>
2014-10-03 11:25:34 -0600

    Show final general response field in human survey. Fix #175

--- amgut/lib/human_survey_supp.py
+++ amgut/lib/human_survey_supp.py
@@ -63,6 +63,8 @@ supplemental_map = {
         ((1,), 'SUPPLEMENTAL_MEDICATION'),  # triggered on Yes
     'HEALTH_QUESTION_51.23':
         ((2, 3, 4,), 'SUPPLEMENTAL_OTHER_CONDITIONS'),  # triggered on Other
+    'DETAILED_DIET_QUESTION_77':
+        ((1, 2, 3, 4, 5, 6), 'SUPPLEMENTAL_OPEN_COMMENT')  # Triggered on any
     }
 
 # associates the question with its type, where SINGLE is a question with only
