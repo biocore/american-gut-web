22275f8ca1e14a9a8a6f75df9a56e0dfd71c5fc2
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-10-04 15:55:22 -0600

    Add test credentials to populate_test_db.sql

--- amgut/db/populate_test.sql
+++ amgut/db/populate_test.sql
@@ -1,1 +1,4 @@
 -- Populates the test database
+
+insert into ag_login (ag_login_id) values ('d8592c74-7da1-2135-e040-8a80115d6401');
+insert into ag_kit (ag_login_id, supplied_kit_id, swabs_per_kit, kit_password, kit_verified) values ('d8592c74-7da1-2135-e040-8a80115d6401', 'test', 1, '%^&*test', 'y');
