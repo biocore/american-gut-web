4ea6fcba44cf0e7e5c28fba97879df18b1cd6441
Joshua Shorenstein <squirrelo@gmail.com>
2015-07-20 16:40:53 -0700

    make DB changes for external survey storage

---
+++ amgut/db/patches/0010.sql
@@ -0,0 +1,22 @@
+CREATE TABLE ag.external_survey ( 
+	external_survey_id   bigserial  NOT NULL,
+	external_survey      varchar  NOT NULL,
+	external_survey_description varchar  ,
+	CONSTRAINT pk_external_surveys PRIMARY KEY ( external_survey_id )
+ );
+
+CREATE TABLE ag.external_survey_answers ( 
+	survey_id            varchar  NOT NULL,
+	external_survey_id   bigint  NOT NULL,
+	pulldown_date        date  NOT NULL,
+	answers              json  NOT NULL,
+	CONSTRAINT pk_third_party_surveys PRIMARY KEY ( survey_id )
+ );
+
+CREATE INDEX idx_external_survey ON ag.external_survey_answers ( external_survey_id );
+
+COMMENT ON TABLE ag.external_survey_answers IS 'This stores answers to third party surveys in JSON format, keyed to the original survey ID';
+
+ALTER TABLE ag.external_survey_answers ADD CONSTRAINT fk_third_party_surveys FOREIGN KEY ( survey_id ) REFERENCES ag.ag_login_surveys( survey_id );
+
+ALTER TABLE ag.external_survey_answers ADD CONSTRAINT fk_external_survey FOREIGN KEY ( external_survey_id ) REFERENCES ag.external_survey( external_survey_id );
