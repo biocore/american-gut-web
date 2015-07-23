a14b5737387ef8f9a7d352b19e66cc4d6f7a9cb3
Joshua Shorenstein <squirrelo@gmail.com>
2015-07-23 12:21:24 -0700

    store pet survey in DB

--- amgut/handlers/animal_survey.py
+++ amgut/handlers/animal_survey.py
@@ -1,13 +1,15 @@
 from urllib import urlencode
-from json import loads
+from json import loads, dumps
+import binascii
+import os
 
 from tornado.web import authenticated
 from tornado.websocket import WebSocketHandler
 
 from amgut.handlers.base_handlers import BaseHandler
 from amgut.lib.survey_supp import primary_animal_survey
-from amgut.lib.util import make_survey_class
-from amgut.connections import ag_data
+from amgut.lib.util import make_survey_class, store_survey
+from amgut.connections import ag_data, redis
 from amgut import text_locale, media_locale
 
 
@@ -25,65 +27,32 @@ class AnimalSurveyHandler(BaseHandler):
     def post(self):
         skid = self.current_user
         tl = text_locale['handlers']
-        participant_name = self.get_argument('animal_name')
-
-       # Add values to tables
-        singles = {}
-        singles['type'] = self.get_argument('type', default=None)
-        singles['origin'] = self.get_argument('origin', default=None)
-        singles['age'] = self.get_argument('age', default=None)
-        singles['gender'] = self.get_argument('gender', default=None)
-        singles['setting'] = self.get_argument('setting', default=None)
-        singles['weight'] = self.get_argument('weight', default=None)
-        singles['diet'] = self.get_argument('diet', default=None)
-        singles['food_source_store'] = self.get_argument('food_source_store',
-                                              default=None)
-        singles['food_source_human'] = self.get_argument('food_source_human',
-                                              default=None)
-        singles['food_source_wild'] = self.get_argument('food_source_wild', default=None)
-        singles['food_type'] = self.get_argument('food_type', default=None)
-        singles['organic_food'] = self.get_argument('organic_food',
-                                                    default=None)
-        singles['grain_free_food'] = self.get_argument('grain_free_food',
-                                                       default=None)
-        singles['living_status'] = self.get_argument('living_status',
-                                                     default=None)
-        singles['outside_time'] = self.get_argument('outside_time',
-                                                    default=None)
-        singles['toilet'] = self.get_argument('toilet', default=None)
-        singles['coprophage'] = self.get_argument('coprophage', default=None)
-        singles['comments'] = self.get_argument('comments', default=None)
-
-        multiples = {k: v[0] for k, v in self.request.body_arguments.items()
-                     if k.startswith('human_') or k.startswith('pet_')}
-
         ag_login_id = ag_data.get_user_for_kit(skid)
-        ag_data.deleteAGParticipantSurvey(ag_login_id, participant_name)
+        ag_login_info = ag_data.get_login_info(ag_login_id)[0]
+        animal_survey_id = binascii.hexlify(os.urandom(8))
 
-        for sample in ag_data.getParticipantSamples(ag_login_id,
-                participant_name):
-            ag_data.deleteSample(sample['barcode'], ag_login_id)
+        form = self.animal_survey()
+        form.process(data=self.request.arguments)
+        data = {'questions': form.data}
+        participant_name = form['Pet_Information_127_0'].data[0]
+        consent = {
+            'login_id': ag_login_id,
+            'participant_name': participant_name,
+            'participant_email': ag_login_info['email'],
+            'assent_obtainer': 'ANIMAL_SURVEY',
+            'parent_1_name': 'ANIMAL_SURVEY',
+            'parent_2_name': 'ANIMAL_SURVEY',
+            'survey_id': animal_survey_id,
+            'is_juvenile': True,
+            'deceased_parent': False,
+            'obtainer_name': 'ANIMAL_SURVEY',
+            'age_range': 'ANIMAL_SURVEY'
+        }
+        redis.hset(animal_survey_id, 'consent', dumps(consent))
+        redis.hset(animal_survey_id, 0, dumps(data))
+        redis.expire(animal_survey_id, 86400)
 
-        # Create the new participant if it doesn't exist (merges)
-        ag_data.addAGAnimalParticipant(ag_login_id, participant_name)
-
-        for field, value in singles.items():
-            if value is None:
-                continue
-
-            ag_data.addAGGeneralValue(ag_login_id, participant_name,
-                                             field, value)
-            ag_data.addAGSingle(ag_login_id, participant_name,
-                                       field, value, 'ag_animal_survey')
-
-        for field, value in multiples.items():
-            if value is None:
-                continue
-
-            ag_data.addAGGeneralValue(ag_login_id, participant_name,
-                                             field, value)
-            ag_data.insertAGMultiple(ag_login_id, participant_name,
-                                            field, value)
+        store_survey(primary_animal_survey, animal_survey_id)
 
         message = urlencode([('errmsg', tl['SUCCESSFULLY_ADDED'] %
                              participant_name)])
