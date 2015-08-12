cb030ed4723117989a26c277439de89735ab7ed4
Joshua Shorenstein <squirrelo@gmail.com>
2015-08-12 12:24:30 -0700

    add patch to delete test barcodes

---
+++ amgut/db/patches/0013.sql
@@ -0,0 +1,45 @@
+--Aug 12, 2015
+-- Remove the test barcodes (900000000 and above) so new barcode system works
+DO $do$
+DECLARE
+	bc varchar;
+	sid varchar;
+	agl uuid;
+	agk uuid;
+	pn varchar;
+BEGIN
+-- Wipe out test barcodes taht are already attached to kits and surveys
+FOR bc, sid IN
+	SELECT barcode, survey_id FROM ag.ag_kit_barcodes WHERE barcode::integer >= 800000000
+LOOP
+	DELETE FROM ag.survey_answers WHERE survey_id = sid;
+	DELETE FROM ag.survey_answers_other WHERE survey_id = sid;
+	DELETE FROM ag.ag_login_surveys WHERE survey_id = sid RETURNING participant_name INTO pn;
+	DELETE from ag.ag_kit_barcodes WHERE barcode = bc RETURNING ag_kit_id INTO agk;
+
+	DELETE FROM barcodes.barcode_exceptions WHERE barcode = bc;
+	DELETE FROM barcodes.plate_barcode WHERE barcode = bc;
+	DELETE FROM barcodes.project_barcode WHERE barcode = bc;
+	DELETE FROM barcodes.barcode WHERE barcode = bc;
+
+	DELETE FROM ag.ag_consent WHERE ag_login_id = (SELECT ag_login_id FROM ag.ag_kit WHERE ag_kit_id = agk) AND participant_name = pn;
+	BEGIN
+		DELETE FROM ag.ag_kit WHERE ag_kit_id = agk;
+	EXCEPTION WHEN foreign_key_violation THEN CONTINUE;
+		--Do nothing on exception because that means there are still barcodes attached to the kit
+	END;
+END LOOP;
+
+-- Wipe out test barcodes that are  attached to handout kits
+FOR bc IN
+	SELECT barcode FROM ag.ag_handout_barcodes WHERE barcode::integer >= 800000000
+LOOP
+	DELETE FROM ag.ag_handout_barcodes WHERE barcode = bc RETURNING kit_id INTO agk;
+	DELETE FROM barcodes.barcode WHERE barcode = bc;
+	BEGIN
+		DELETE FROM ag.ag_handout_kits WHERE kit_id = agk;
+	EXCEPTION WHEN foreign_key_violation THEN CONTINUE;
+		--Do nothing on exception because that means there are still barcodes attached to the kit
+	END;
+END LOOP;
+END $do$;
