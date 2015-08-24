a0635d55d88abee4031b993f81c1026a3e445431
Joshua Shorenstein <squirrelo@gmail.com>
2015-08-24 12:28:56 -0700

    add country to zipcodes table

---
+++ amgut/db/patches/0018.sql
@@ -0,0 +1,7 @@
+-- August 24, 2015
+-- add country column to zipcode table
+ALTER TABLE ag.zipcodes ADD COLUMN country varchar;
+UPDATE ag.zipcodes SET country = 'United States' WHERE state IN ('AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY');
+UPDATE ag.zipcodes SET country = 'Canada' WHERE state IN ('AB','BC','MB','NB','NL','NS','NT','NU','ON','PE','QC','SK','YT');
+-- Delete the rest so the table can repopulate correctly
+DELETE FROM ag.zipcodes WHERE country IS NULL;
