fa687e67f4ad88efe022a7ea7df6e5d023268a02
Joshua Shorenstein <squirrelo@gmail.com>
2015-08-26 08:38:16 -0700

    update all countries by google

--- amgut/db/patches/0018.sql
+++ amgut/db/patches/0018.sql
@@ -23,6 +23,21 @@ UPDATE ag.iso_country_lookup SET country = 'Libya' WHERE country = 'Libyan Arab 
 UPDATE ag.iso_country_lookup SET country = 'Macau' WHERE country = 'Macao';
 UPDATE ag.iso_country_lookup SET country = 'Macedonia (FYROM)' WHERE country = 'Macedonia, The Former Yugoslav Republic of';
 UPDATE ag.iso_country_lookup SET country = 'Tanzania' WHERE country = 'The United Republic of Tanzania';
+UPDATE ag.iso_country_lookup SET country = 'Côte d''Ivoire' WHERE country = 'Cote D''ivoire';
+UPDATE ag.iso_country_lookup SET country = 'The Bahamas' WHERE country = 'Bahamas';
+UPDATE ag.iso_country_lookup SET country = 'The Gambia' WHERE country = 'Gambia';
+UPDATE ag.iso_country_lookup SET country = 'Guinea-Bissau' WHERE country = 'Guinea-bissau';
+UPDATE ag.iso_country_lookup SET country = 'Heard Island and McDonald Islands' WHERE country = 'Heard Island and Mcdonald Islands';
+UPDATE ag.iso_country_lookup SET country = 'Vatican City' WHERE country = 'Holy See (Vatican City State)';
+UPDATE ag.iso_country_lookup SET country = 'Myanmar (Burma)' WHERE country = 'Myanmar';
+UPDATE ag.iso_country_lookup SET country = 'Palestine' WHERE country = 'Palestinian Territory, Occupied';
+UPDATE ag.iso_country_lookup SET country = 'Pitcairn Islands' WHERE country = 'Pitcairn';
+UPDATE ag.iso_country_lookup SET country = 'Saint Vincent and the Grenadines' WHERE country = 'Saint Vincent and The Grenadines';
+UPDATE ag.iso_country_lookup SET country = 'São Tomé and Príncipe' WHERE country = 'Sao Tome and Principe';
+UPDATE ag.iso_country_lookup SET country = 'South Georgia and the South Sandwich Islands' WHERE country = 'South Georgia and The South Sandwich Islands';
+UPDATE ag.iso_country_lookup SET country = 'Timor-Leste' WHERE country = 'Timor-leste';
+UPDATE ag.iso_country_lookup SET country = 'Vietnam' WHERE country = 'Viet Nam';
+UPDATE ag.iso_country_lookup SET country = 'Åland Islands' WHERE country = 'Aland Islands';
 
 -- Add EBI country name column and populate it
 ALTER TABLE ag.iso_country_lookup ADD COLUMN EBI varchar;
@@ -36,3 +51,11 @@ UPDATE ag.iso_country_lookup SET EBI = 'Republic of the Congo' WHERE country = '
 UPDATE ag.iso_country_lookup SET EBI = 'Svalbard' WHERE country = 'Svalbard and Jan Mayen';
 UPDATE ag.iso_country_lookup SET EBI = 'USA' WHERE country = 'United States';
 UPDATE ag.iso_country_lookup SET EBI = 'Virgin Islands' WHERE country = 'U.S. Virgin Islands';
+UPDATE ag.iso_country_lookup SET EBI = 'Cote D''ivoire' WHERE country = 'Côte d''Ivoire';
+UPDATE ag.iso_country_lookup SET EBI = 'Bahamas' WHERE country = 'The Bahamas';
+UPDATE ag.iso_country_lookup SET EBI = 'Gambia' WHERE country = 'The Gambia';
+UPDATE ag.iso_country_lookup SET EBI = 'Myanmar' WHERE country = 'Myanmar (Burma)';
+UPDATE ag.iso_country_lookup SET EBI = 'Sao Tome and Principe' WHERE country = 'São Tomé and Príncipe';
+UPDATE ag.iso_country_lookup SET EBI = 'Viet Nam' WHERE country = 'Vietnam';
+UPDATE ag.iso_country_lookup SET EBI = 'Aland Islands' WHERE country = 'Åland Islands';
+
