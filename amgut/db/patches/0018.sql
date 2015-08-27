e0fa7ebc4dad3e50649fcfe0f99e28ccafcfb634
Joshua Shorenstein <squirrelo@gmail.com>
2015-08-27 11:30:26 -0700

    fix syntax

--- amgut/db/patches/0018.sql
+++ amgut/db/patches/0018.sql
@@ -5,4 +5,4 @@ ALTER TABLE ag.zipcodes ALTER COLUMN country DROP DEFAULT;
 UPDATE ag.zipcodes SET country = 'United States' WHERE state IN ('AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY');
 UPDATE ag.zipcodes SET country = 'Canada' WHERE state IN ('AB','BC','MB','NB','NL','NS','NT','NU','ON','PE','QC','SK','YT');
 -- Delete the rest so the table can repopulate correctly
-DELETE FROM ag.zipcodes WHERE country IS '';
+DELETE FROM ag.zipcodes WHERE country = '';
