ee443725d1590574b4d124f06c15815eb5f4f46b
Joshua Shorenstein <squirrelo@gmail.com>
2015-09-30 20:23:30 -0700

    transition some more things

--- amgut/lib/startup_tests.py
+++ amgut/lib/startup_tests.py
@@ -1,4 +1,4 @@
-from amgut.lib.data_access.sql_connection import SQLConnectionHandler
+from amgut.lib.data_access.sql_connection import TRN
 from os import listdir
 from os.path import join, dirname, abspath
 
@@ -11,25 +11,26 @@ def startup_tests():
 
 def patch_number():
     # Make sure the system is using the latest patch before starting up.
-    conn_handler = SQLConnectionHandler()
-    sql = "SELECT current_patch FROM ag.settings"
-    system = conn_handler.execute_fetchone(sql)[0]
-    patches_dir = join(dirname(abspath(__file__)), '../db/patches')
-    latest = sorted(listdir(patches_dir)).pop()
-    if latest != system:
-        raise EnvironmentError(("Not running latest patch. System: %s "
-                                "Latest: %s") % (system, latest))
+    with TRN:
+        TRN.add('SELECT current_patch FROM ag.settings')
+        system = TRN.execute_fetchlast()
+        patches_dir = join(dirname(abspath(__file__)), '../db/patches')
+        latest = sorted(listdir(patches_dir)).pop()
+        if latest != system:
+            raise EnvironmentError(("Not running latest patch. System: %s "
+                                    "Latest: %s") % (system, latest))
 
 
 def barcodes_correct():
     # For patch 0011 & 0012
     # Needed because barcodes are added as last barcode in system + 1
     # and system testing was using these larger barcodes. Now use 0-1000 range
-    conn_handler = SQLConnectionHandler()
-    sql = """SELECT barcode
-             FROM barcodes.barcode
-             WHERE barcode::integer >= 800000000"""
-    bcs = conn_handler.execute_fetchall(sql)
-    if bcs:
-        joined_bcs = ", ".join([x[0] for x in bcs])
-        raise EnvironmentError("Invalid barcodes found: %s" % joined_bcs)
+    with TRN:
+        sql = """SELECT barcode
+                 FROM barcodes.barcode
+                 WHERE barcode::integer >= 800000000"""
+        TRN.add(sql)
+        bcs = TRN.execute_fetchindex()
+        if bcs:
+            joined_bcs = ", ".join([x[0] for x in bcs])
+            raise EnvironmentError("Invalid barcodes found: %s" % joined_bcs)
