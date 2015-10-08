5d46555447072c9e81dcd60ad823245fb098d039
Jamie Morton <jamietmorton@gmail.com>
2015-10-08 16:25:58 -0700

    TST: Adding in animal participant test

--- amgut/test/test_add_sample_overview.py
+++ amgut/test/test_add_sample_overview.py
@@ -11,22 +11,38 @@ class TestAddSampleOverview(TestHandlerBase):
         self.assertTrue(
             response.effective_url.endswith('/authed/add_sample_overview'))
 
-    def test_get_overview(self):
+    def test_get_overview_human(self):
+        # Test with human login id
         self.mock_login('tst_LbxUH')
         response = self.get(
             '/authed/add_sample_overview/')
         self.assertEqual(response.code, 200)
         self.assertTrue(
             response.effective_url.endswith('/authed/add_sample_overview/'))
-
         # Check for some main text
         self.assertIn('sample source', response.body)
-        self.assertIn('REMOVED-2', response.body)
-        self.assertIn('REMOVED-3', response.body)
-        self.assertIn('REMOVED-0', response.body)
-        self.assertIn('REMOVED-1', response.body)
+        self.assertIn('REMOVED-2 - Human Source', response.body)
+        self.assertIn('REMOVED-3 - Human Source', response.body)
+        self.assertIn('REMOVED-0 - Human Source', response.body)
+        self.assertIn('REMOVED-1 - Human Source', response.body)
         self.assertIn('Environmental', response.body)
-        self.assertIn('Human Source', response.body)
+        self.assertNotIn('- Animal Source', response.body)
+
+    def test_get_overview_animal(self):
+        # Test with animal login id
+        self.mock_login('tst_DbGvP')
+        response = self.get(
+            '/authed/add_sample_overview/')
+        self.assertEqual(response.code, 200)
+        self.assertTrue(
+            response.effective_url.endswith('/authed/add_sample_overview/'))
+        # Check for some main text
+        self.assertIn('sample source', response.body)
+        self.assertIn('REMOVED-0 - Animal Source', response.body)
+        self.assertIn('REMOVED-0 - Human Source', response.body)
+        self.assertIn('REMOVED-1 - Human Source', response.body)
+        self.assertIn('Environmental', response.body)
+
 
 if __name__ == '__main__':
     main()
