c7a85974e654ce8e6ec67406e10b6b937cc0b039
Joshua Shorenstein <squirrelo@gmail.com>
2016-03-22 11:52:57 -0700

    fix for safari

---
+++ amgut/lib/data_access/constants.py
@@ -0,0 +1,16 @@
+collapse_human_sites = {
+    'Stool': 'stool',
+    'Mouth': 'oral',
+    'Right hand': 'skin',
+    'Left hand': 'skin',
+    'Forehead': 'skin',
+    'Torso': 'skin',
+    'Left leg': 'skin',
+    'Right leg': 'skin',
+    'Nares': '',
+    'Hair': '',
+    'Tears': '',
+    'Nasal mucus': '',
+    'Ear wax': '',
+    'Vaginal mucus': 'vaginal'
+    }
