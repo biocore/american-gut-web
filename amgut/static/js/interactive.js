c76f469a0760db0ae8bdd47d346260b3241c2956
geierj <justin.alexander.geier@gmail.com>
2017-09-08 13:11:25 -0700

    move modal js to js/interactive.js

--- amgut/static/js/interactive.js
+++ amgut/static/js/interactive.js
@@ -437,7 +437,7 @@ function add_metadata_barchart() {
                                                  data, $('#collapse').val());
       barChartSummaryData.labels.push(title);
       window.summaryBar.update();
-      var rem = $("<td><a href='#' onclick='remove_sample(\"" + title + "\"); return false;'>Remove</a></td>");
+      var rem = $("<td><button href='#' onclick='remove_sample(\"" + title + "\"); return false;'>X</button></td>");
       $('#remove-row').append(rem);
     })
     .fail(function() {
@@ -478,4 +478,96 @@ function remove_sample(title) {
   window.summaryBar.update();
   $('td').eq(data_pos).remove();
   return false;
+};
+
+
+// Get the modal
+var modal1 = document.getElementById('modal-one');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("modal-button-one");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks the button, open the modal
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+};
+
+
+
+// Get the modal
+var modal2 = document.getElementById('modal-two');
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("modal-button-two");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+// Get the modal
+var modal3 = document.getElementById('modal-three');
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("modal-button-three");
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close3")[0];
+
+
+// When the user clicks the button, open the modal
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
 };
