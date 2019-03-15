0e95adc651d6789aa5274d62f63dfa3c7b762e31
Daniel McDonald <danielmcdonald@ucsd.edu>
2019-03-15 09:16:55 -0700

    Added field

--- amgut/db/patches/0047.sql
+++ amgut/db/patches/0047.sql
@@ -2,6 +2,7 @@
 -- labadmin
 CREATE TABLE barcodes.project_qiita_buffer (
     barcode varchar,
+    pushed_to_qiita char default 'N',
     CONSTRAINT pk_project_qiita_buffer PRIMARY KEY (barcode),
     CONSTRAINT fk_project_qiita_buffer FOREIGN KEY (barcode) REFERENCES barcodes.barcode(barcode)
 );
